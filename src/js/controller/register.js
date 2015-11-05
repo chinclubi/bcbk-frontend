d6606b951a80d18eaad49a96bc13d1064ea50843
Sorrawit Chancherngkit <ongoing@outlook.com>
2015-11-06 00:34:42 +0700

    implement edit user function

--- src/js/controller/register.js
+++ src/js/controller/register.js
@@ -11,20 +11,19 @@
     var successForm = $('.regisSuccess')
     successForm.hide()
     $(document).ready(function () {
-      var regisContent = $('.regisContent')
-      var firstnameE = $('#firstname')
-      var lastnameE = $('#lastname')
-      var genderE = $('#gender')
-      var professionE = $('#profession')
-      var workplaceE = $('#workplace')
-      var emailE = $('#email')
-      var twitterE = $('#twitter')
-      var websiteE = $('#website')
-      var sizeE = $('#shirt-size')
-      var food_reqE = $('#food-requirement')
-      var food_allergyE = $('#allergy')
-      var interestE = $('#interest')
-      var elementArr = [firstnameE, lastnameE, genderE, professionE, workplaceE, emailE, sizeE, food_reqE, food_allergyE, interestE]
+      var regisContent = $('.regisContent');
+      var firstnameE = $('#firstname');
+      var lastnameE = $('#lastname');
+      var genderE = $('#gender');
+      var professionE = $('#profession');
+      var workplaceE = $('#workplace');
+      var emailE = $('#email');
+      var twitterE = $('#twitter');
+      var websiteE = $('#website');
+      var food_reqE = $('#food-requirement');
+      var food_allergyE = $('#allergy');
+      var interestE = $('#interest');
+      var elementArr = [firstnameE, lastnameE, genderE, professionE, workplaceE, emailE, food_reqE, food_allergyE, interestE]
 
       $('.regis-btn').click(function () {
         var isEmpty = false
@@ -45,7 +44,6 @@
           var emailx = emailE.val()
           var twitterx = twitterE.val()
           var websitex = emailE.val()
-          var sizex = sizeE.val()
           var food_reqx = food_reqE.val()
           var food_allergyx = food_allergyE.val()
           var interestx = interestE.val()
@@ -59,20 +57,12 @@
             email: emailx,
             twitter: twitterx,
             website: websitex,
-            size: sizex,
             food_req: food_reqx,
             food_allergy: food_allergyx,
             interest: interestx
           }
-          console.log(sendingData)
-          regisContent.fadeOut()
+          regisContent.fadeOut();
           // successForm.fadeIn()
-
-          // $http.post('http://api.barcampbangkhen.org/register', sendingData).then(function(response){
-          //   successForm.fadeIn()
-          // }, function(xhr){
-          //    console.log(xhr.data)
-          // })
 
           $.post(
             'http://api.barcampbangkhen.org/register', sendingData,
