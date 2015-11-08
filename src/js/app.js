ee95d6d3407c12a438c08af9b50e623b2a141ecf
ChinCluBi <chincub@gmail.com>
2015-11-08 17:00:19 +0700

    Add real google map

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,13 @@
 /* global angular */
 
-;
-(function () {
-    angular
-        .module('bcbk-app', [
-            'services.route',
-            'controller.homepage',
-            'controller.register',
-            'controller.whoscoming',
-            'controller.edituser',
-            // 'directive.navbar'
-        ])
+;(function () {
+  angular
+    .module('bcbk-app', [
+      'services.route',
+      'controller.homepage',
+      'controller.register',
+      'controller.whoscoming',
+      'controller.edituser'
+    // 'directive.navbar'
+    ])
 })()
