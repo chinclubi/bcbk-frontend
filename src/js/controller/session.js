cf9bb291946d160c980bc3e0f6304aa9e5724908
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2015-11-19 11:29:04 +0700

    session page

--- src/js/controller/session.js
+++ src/js/controller/session.js
@@ -10,5 +10,21 @@
 
     SessionController.$inject = ['$scope', '$window']
     function SessionController($scope, $window) {
+
+        $scope.sessions = [];
+        $scope.rooms = ['17201', '17302', '17303', '17304', '17401', '17402'];
+        $scope.favorites = JSON.parse(localStorage.favorite || '{}');
+        $scope.timeslots = [
+            '10:40 - 11:10',
+            '11:10 - 11:40',
+            '11:40 - 12:10',
+            '13:00 - 13:30',
+            '13:30 - 14:00',
+            '14:00 - 14:30',
+            '14:30 - 15:00',
+            '15:20 - 15:50',
+            '15:50 - 16:20',
+            '16:20 - 16:50'
+        ];
     }
 })()
